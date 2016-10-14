a0eefe8e249cad4744f72c10f6c11f04dae4b321
Jonathan Barton (MHE) <jonathan.barton@mheducation.com>
2016-10-13 22:40:25 -0400

    fixes file structure for index.js

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ var glob = require('glob-fs')({ gitignore: true });
 var fs = require('fs');
 var path = require('path');
 
-function getJsonFilesAsObject(fileFolder) {
-	var files = glob.readdirSync(`lib/static/${fileFolder}/*.json`, {});
+function getJsonFilesFromFolder(fileFolder) {
+	var filePath = `static/${fileFolder}/*.json`;
+	var files = glob.readdirSync(filePath, {cwd: __dirname});
 	var filesObject = {};
 	files.forEach(function(file){
 		var basename = path.basename(file, '.json');
 		var fileContent = fs.readFileSync(file);
 		filesObject[basename] = JSON.parse(fileContent);
 	});
-	return filesObject;
+	return filesObject; 
 }
 
 module.exports = {
-	telemetry: getJsonFilesAsObject('telemetry')
+	telemetry: getJsonFilesFromFolder('telemetry')
 }
